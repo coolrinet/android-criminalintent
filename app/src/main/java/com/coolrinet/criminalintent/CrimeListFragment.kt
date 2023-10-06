b581e6fb1858ea9e41cb2f85e75a661130388520
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-10-06 14:52:19 +0700

    updated CrimeListFragment with coroutine using

--- app/src/main/java/com/coolrinet/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/coolrinet/criminalintent/CrimeListFragment.kt
@@ -1,16 +1,17 @@
 package com.coolrinet.criminalintent
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.coolrinet.criminalintent.databinding.FragmentCrimeListBinding
-
-private const val TAG = "CrimeListFragment"
+import kotlinx.coroutines.launch
 
 class CrimeListFragment : Fragment() {
     private var _binding: FragmentCrimeListBinding? = null
@@ -21,11 +22,6 @@ class CrimeListFragment : Fragment() {
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,11 +31,18 @@ class CrimeListFragment : Fragment() {
 
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+            }
+        }
     }
 
     override fun onDestroyView() {
