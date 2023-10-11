b48af1a92dd3288dffd27eabca4f9a90e6d35104
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-10-11 17:34:46 +0700

    created CrimeDetailViewModel and CrimeDetailViewModelFactory

--- app/src/main/java/com/coolrinet/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/coolrinet/criminalintent/CrimeDetailFragment.kt
@@ -7,14 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.coolrinet.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.util.UUID
-
-private const val TAG = "CrimeDetailFragment"
 
 class CrimeDetailFragment : Fragment() {
     private var _binding: FragmentCrimeDetailBinding? = null
@@ -23,21 +22,10 @@ class CrimeDetailFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private lateinit var crime: Crime
-
     private val args: CrimeDetailFragmentArgs by navArgs()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        crime = Crime(
-            id = UUID.randomUUID(),
-            title = "",
-            date = Date(),
-            isSolved = false
-        )
-
-        Log.d(TAG, "The crime ID is: ${args.crimeId}")
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
     }
 
     override fun onCreateView(
@@ -54,16 +42,16 @@ class CrimeDetailFragment : Fragment() {
 
         binding.apply {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
+
             }
 
             crimeDate.apply {
-                text = SimpleDateFormat("EEEE, MMMM d, y", Locale.US).format(crime.date)
+
                 isEnabled = false
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
+
             }
         }
     }
