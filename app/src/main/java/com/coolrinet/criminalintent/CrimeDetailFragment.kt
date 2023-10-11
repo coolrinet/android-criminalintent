024be9267d8f7749b0c41892dd11b15e122582bc
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-10-11 18:16:10 +0700

    added untitled crime check in CrimeDetailFragment

--- app/src/main/java/com/coolrinet/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/coolrinet/criminalintent/CrimeDetailFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.coolrinet.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
@@ -27,6 +30,24 @@ class CrimeDetailFragment : Fragment() {
 
     private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
         CrimeDetailViewModelFactory(args.crimeId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+            if (binding.crimeTitle.text.isNullOrEmpty()) {
+                Toast.makeText(
+                    activity,
+                    R.string.no_untitled_crime_warning,
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@addCallback
+            }
+            findNavController().popBackStack()
+        }
+
+        callback.isEnabled = true
     }
 
     override fun onCreateView(
