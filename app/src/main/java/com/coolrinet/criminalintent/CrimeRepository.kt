4782f3c63c632375c057ffc3048142c0794aaf3a
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-10-15 18:19:17 +0700

    added addCrime implementation in database

--- app/src/main/java/com/coolrinet/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/coolrinet/criminalintent/CrimeRepository.kt
@@ -26,11 +26,13 @@ class CrimeRepository private constructor(
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
     suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
-
     fun updateCrime(crime: Crime) {
         coroutineScope.launch {
             database.crimeDao().updateCrime(crime)
         }
+    }
+    suspend fun addCrime(crime: Crime) {
+        database.crimeDao().addCrime(crime)
     }
 
     companion object {
